c5431d1466528c585da7aff051505348bdb96b96
Tobias Hassenkloever <Tobi@Cookiebook.local>
2015-05-23 13:40:31 +0200

    Added simple echo method on post for server

--- player.js
+++ player.js
@@ -1,15 +1,40 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var bodyParser = require('body-parser');
+var multer = require('multer'); 
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
+
+var SUCCESS = "success";
+var FAILURE = "failure";
+
+function playURL(url) {
+
+	// TODO play stuff
+ 	console.log('Just received URL: ' + url + ' for playback');
+
+	return SUCCESS;
+}
+
+app.get('/status', function (req, res) {
+  res.send('Everything is OK!');
 });
 
-var server = app.listen(3000, function () {
+app.post('/play', function (req, res) {
+
+	// echo body to client
+ 	console.log(req.body);
+ 	res.json(req.body);
+});
+
+var server = app.listen(1337, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s%s', host, port);
 
 });
