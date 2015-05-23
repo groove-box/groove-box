55d7ea2766057dc8dd3562341e4fbbbaa9e614ea
Joschka Schulz <Horstyschulz@gmail.com>
2015-05-23 17:13:05 +0200

    resolve mp3 url

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 
 var SoundcloudWrapper = require('./libs/soundcloud_wrapper');
+var PlayerWrapper = require('./libs/player_wrapper');
+var Server = require('./libs/server');
+
+var config = require('./config/config.json');
+var request = require('request');
 
 var sc = new SoundcloudWrapper();
+var playerWrapper = new PlayerWrapper();
 sc.init();
 
 function urlReceivedCallback (url) {
  	console.log('Just received URL: \'' + url + '\' for playback');
   sc.resolve(url, function(resolvedObject) {
-    console.log("Body: ", resolvedObject);
+    request(resolvedObject.stream_url + '?client_id=' + config.client_id, function (error, response, body) {
+      playerWrapper.add(response['request'].uri.href);
+    });
   });
 }
 
-var Server = require('./libs/server');
 
 var myServer = new Server(1337);
 myServer.init(urlReceivedCallback);
 
 myServer.start();
-
-//var PlayerWrapper = require('./libs/player_wrapper');
-//var file = __dirname + '/demo2.mp3';
-// var player = new Player(file);
-//var playerWrapper = new PlayerWrapper();
-
-//playerWrapper.add(file);
-
-// player.play();
-
-
-
-// setTimeout(function(){
-// 	console.log(file);
-// 	// player.add(file);
-// 	// player.play();
-// }, 1000);
