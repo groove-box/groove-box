00c16662a41c7210a66ee2e691c652efb40f91bc
Joschka Schulz <Horstyschulz@gmail.com>
2015-05-23 17:45:19 +0200

    change the resolve url from index to soudcloud

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ var SoundcloudWrapper = require('./libs/soundcloud_wrapper');
 var PlayerWrapper = require('./libs/player_wrapper');
 var Server = require('./libs/server');
 
-var config = require('./config/config.json');
-var request = require('request');
 
 var sc = new SoundcloudWrapper();
 var playerWrapper = new PlayerWrapper();
 sc.init();
 
 function urlReceivedCallback (url) {
- 	console.log('Just received URL: \'' + url + '\' for playback');
-  sc.resolve(url, function(resolvedObject) {
-    request(resolvedObject.stream_url + '?client_id=' + config.client_id, function (error, response, body) {
-      playerWrapper.add(response['request'].uri.href);
-    });
+ 	console.log('Just received URL: \'', url, '\' for playback');
+  sc.resolve(url, function(mp3Url) {
+    console.log("Added resolved URL: ", mp3Url)
+    playerWrapper.add(mp3Url);
   });
 }
 
