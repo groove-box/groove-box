2a42f73b48691f556eff7787786c64d93d683b72
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2015-05-24 11:21:55 +0200

    display the list

--- libs/player_wrapper.js
+++ libs/player_wrapper.js
@@ -5,231 +5,240 @@ var util = require('util');
 var player;
 
 function PlayerWrapper() {
-	this.player_initialized = false;
-	this.player = null;
-	this.playlist = [];
-	this.playlistData = {};
-	this.history = [];
-	this.tweeter = new Tweeter();
-	this.soundcloudWrapper = new SoundcloudWrapper();
+    this.player_initialized = false;
+    this.player = null;
+    this.playlist = [];
+    this.playlistData = {};
+    this.history = [];
+    this.tweeter = new Tweeter();
+    this.soundcloudWrapper = new SoundcloudWrapper();
 }
 
-PlayerWrapper.prototype.init = function() {
-	if (this.isInitialized()) {
-		return;
-	}
-	this.player_initialized = true;
-	var self = this;
+PlayerWrapper.prototype.init = function () {
+    if (this.isInitialized()) {
+        return;
+    }
+    this.player_initialized = true;
+    var self = this;
 
-	// Initialize with empty playlist.
-	this.player = new Player([])
-	// Enable streaming.
-		.enable('stream')
-		.on('error', this.errorHandler)
-		.on('playing', function() {
-			self.playStartHandler.apply(self, arguments);
-		})
-		.on('playend', function() {
-			self.playEndHandler.apply(self, arguments);
-		});
+    // Initialize with empty playlist.
+    this.player = new Player([])
+        // Enable streaming.
+            .enable('stream')
+            .on('error', this.errorHandler)
+            .on('playing', function () {
+                self.playStartHandler.apply(self, arguments);
+            })
+            .on('playend', function () {
+                self.playEndHandler.apply(self, arguments);
+            });
 
-		this.player.next = function() {};
-}
+    this.player.next = function () {
+    };
+};
 
-PlayerWrapper.prototype.errorHandler = function(err) {
-	console.log(err);
-}
+PlayerWrapper.prototype.errorHandler = function (err) {
+    console.log(err);
+};
 
-PlayerWrapper.prototype.playStartHandler = function(item) {
-	var status = util.format(
-		'Currently playing: %s. Check it out: %s',
-		item.track_data.title,
-		item.track_data.permalink_url
-	);
-	this.tweeter.tweet(status);
-}
+PlayerWrapper.prototype.playStartHandler = function (item) {
+    var status = util.format(
+            'Currently playing: %s. Check it out: %s',
+            item.track_data.title,
+            item.track_data.permalink_url
+    );
+    this.tweeter.tweet(status);
+};
 
-PlayerWrapper.prototype.playEndHandler = function(item) {
-	item.played++;
-	item.finished = true;
-	this.playNextTrack();
-}
+PlayerWrapper.prototype.playEndHandler = function (item) {
+    item.played++;
+    item.finished = true;
+    this.playNextTrack();
+};
 
-PlayerWrapper.prototype.isInitialized = function() {
-	return this.player_initialized;
-}
+PlayerWrapper.prototype.isInitialized = function () {
+    return this.player_initialized;
+};
 
-PlayerWrapper.prototype.isPlaying = function() {
-	var currentTrack = this.getCurrentTrack();
+PlayerWrapper.prototype.isPlaying = function () {
+    var currentTrack = this.getCurrentTrack();
 
-	var result = !!currentTrack && (currentTrack.finished === false);
+    var result = !!currentTrack && (currentTrack.finished === false);
 
-	return result;
-}
+    return result;
+};
 
-PlayerWrapper.prototype.getCurrentTrack = function() {
-	var result = this.player.playing;
+PlayerWrapper.prototype.getCurrentTrack = function () {
+    var result = this.player.playing;
 
-	return result;
-}
+    return result;
+};
 
-PlayerWrapper.prototype.getNextTrack = function() {
-	var nextTrackIndex = this.getNextTrackIndex();
+PlayerWrapper.prototype.getNextTrack = function () {
+    var nextTrackIndex = this.getNextTrackIndex();
 
-	var result = this.player._list[nextTrackIndex];
+    var result = this.player._list[nextTrackIndex];
 
-	return result;
-}
+    return result;
+};
 
-PlayerWrapper.prototype.getCurrentTrackIndex = function() {
-	return (this.getNextTrackIndex() - 1);
-}
+PlayerWrapper.prototype.getCurrentTrackIndex = function () {
+    return (this.getNextTrackIndex() - 1);
+};
 
-PlayerWrapper.prototype.getNextTrackIndex = function() {
-	return this.player.history.length;
-}
+PlayerWrapper.prototype.getNextTrackIndex = function () {
+    return this.player.history.length;
+};
 
-PlayerWrapper.prototype.add = function(data) {
-	if (!this.isInitialized()) {
-		this.init();
-	}
+PlayerWrapper.prototype.add = function (data) {
+    if (!this.isInitialized()) {
+        this.init();
+    }
 
-	var self = this;
+    var self = this;
 
-	this.player.add(data);
+    this.player.add(data);
 
-	if (!this.isPlaying()) {
-		this.playNextTrack()
-	}
-}
+    if (!this.isPlaying()) {
+        this.playNextTrack()
+    }
+};
 
-PlayerWrapper.prototype.addToPlaylist = function(data) {
-	if (!this.isInitialized()) {
-		this.init();
-	}
+PlayerWrapper.prototype.addToPlaylist = function (data) {
+    if (!this.isInitialized()) {
+        this.init();
+    }
 
-	var playlistItem = this.getPlaylistItem(data);
-	var is_new_entry = !playlistItem;
-	if (is_new_entry) {
-		playlistItem = this._generateNewPlaylistItem(data);
-	}
-	else {
-		playlistItem.votes++;
-		playlistItem.finished = false;
-	}
+    var playlistItem = this.getPlaylistItem(data);
+    var is_new_entry = !playlistItem;
+    if (is_new_entry) {
+        playlistItem = this._generateNewPlaylistItem(data);
+    }
+    else {
+        playlistItem.votes++;
+        playlistItem.finished = false;
+    }
 
-	if (is_new_entry) {
-		this.add(playlistItem);
-	}
-	else {
-		this._sortPlaylistByVotes();
-	}
-}
+    if (is_new_entry) {
+        this.add(playlistItem);
+    }
+    else {
+        this._sortPlaylistByVotes();
+    }
+    this.outputPlaylist();
+};
 
-PlayerWrapper.prototype.prepareNextTrack = function(callback) {
-	var nextTrack = this.getNextTrack();
+PlayerWrapper.prototype.outputPlaylist = function () {
+    for (var i = 0; i < this.player._list.length; i++) {
+        var cur = this.player._list[i];
+        console.log(i + '. ' + cur.track_data.title);
+    }
+};
 
-	this.prepareTrack(nextTrack, callback);
-}
+PlayerWrapper.prototype.prepareNextTrack = function (callback) {
+    var nextTrack = this.getNextTrack();
 
-PlayerWrapper.prototype.prepareTrack = function(item, callback) {
-	var self = this;
+    this.prepareTrack(nextTrack, callback);
+};
 
-	if (item) {
-		this.soundcloudWrapper.resolveStreamUrl(item.track_data.stream_url, function(resolved_stream_url) {
-			item[self.player.options.src] = resolved_stream_url;
+PlayerWrapper.prototype.prepareTrack = function (item, callback) {
+    var self = this;
 
-			if (callback) {
-				callback.apply(self);
-			}
-		});
-	}
-	// @COMMENT: I guess this is bad, but currently it just works.
-	else if(callback) {
-		callback.apply(self);
-	}
-}
+    if (item) {
+        this.soundcloudWrapper.resolveStreamUrl(item.track_data.stream_url, function (resolved_stream_url) {
+            item[self.player.options.src] = resolved_stream_url;
 
-PlayerWrapper.prototype.playNextTrack = function() {
-	this.prepareNextTrack(
-		function() {
-			this._playerNext();
-		}
-	);
-}
+            if (callback) {
+                callback.apply(self);
+            }
+        });
+    }
+    // @COMMENT: I guess this is bad, but currently it just works.
+    else if (callback) {
+        callback.apply(self);
+    }
+};
 
-PlayerWrapper.prototype._playerNext = function() {
-	var nextTrackIndex = this.getNextTrackIndex();
-	var player = (this._list) ? this : this.player;
+PlayerWrapper.prototype.playNextTrack = function () {
+    this.prepareNextTrack(
+            function () {
+                this._playerNext();
+            }
+    );
+};
 
-	if (nextTrackIndex >= player._list.length) {
-		player.emit('error', 'No next song was found');
-		player.emit('finish', player.playing);
-		return player;
-	}
+PlayerWrapper.prototype._playerNext = function () {
+    var nextTrackIndex = this.getNextTrackIndex();
+    var player = (this._list) ? this : this.player;
 
-	player.stop();
-	player.play(nextTrackIndex);
+    if (nextTrackIndex >= player._list.length) {
+        player.emit('error', 'No next song was found');
+        player.emit('finish', player.playing);
+        return player;
+    }
 
-	return player;
-}
+    player.stop();
+    player.play(nextTrackIndex);
 
-PlayerWrapper.prototype.getPlaylistItem = function(data) {
-	var result;
+    return player;
+};
 
-	for (var i = 0; i < this.player._list.length; i++) {
-		var cur = this.player._list[i];
-		if (cur.track_data.id === data.track_data.id) {
-			result = cur;
-			break;
-		}
-	}
+PlayerWrapper.prototype.getPlaylistItem = function (data) {
+    var result;
 
-	return result;
-}
+    for (var i = 0; i < this.player._list.length; i++) {
+        var cur = this.player._list[i];
+        if (cur.track_data.id === data.track_data.id) {
+            result = cur;
+            break;
+        }
+    }
 
-PlayerWrapper.prototype._generateNewPlaylistItem = function(data) {
-	var result = data;
+    return result;
+};
 
-	result.finished = false;
-	result.played = 0;
-	result.votes = 1;
+PlayerWrapper.prototype._generateNewPlaylistItem = function (data) {
+    var result = data;
 
-	return result;
-}
+    result.finished = false;
+    result.played = 0;
+    result.votes = 1;
 
-PlayerWrapper.prototype._sortPlaylistByVotes = function() {
-	var playlist = this.player._list;
-	if (playlist.length > 1) {
-		var nextTrackIndex = this.getNextTrackIndex();
-		// Get actual sortable part of the playlist.
-		var sortablePlaylistPart = playlist.slice(nextTrackIndex);
+    return result;
+};
 
-		if (sortablePlaylistPart.length > 0) {
-			sortablePlaylistPart.sort(this._sortPlaylistByVotesCallback);
+PlayerWrapper.prototype._sortPlaylistByVotes = function () {
+    var playlist = this.player._list;
+    if (playlist.length > 1) {
+        var nextTrackIndex = this.getNextTrackIndex();
+        // Get actual sortable part of the playlist.
+        var sortablePlaylistPart = playlist.slice(nextTrackIndex);
 
-			var parametersForSplice = sortablePlaylistPart;
-			parametersForSplice.unshift(nextTrackIndex, playlist.length);
+        if (sortablePlaylistPart.length > 0) {
+            sortablePlaylistPart.sort(this._sortPlaylistByVotesCallback);
 
-			Array.prototype.splice.apply(
-				playlist,
-				parametersForSplice
-			);
-		}
-	}
-}
+            var parametersForSplice = sortablePlaylistPart;
+            parametersForSplice.unshift(nextTrackIndex, playlist.length);
 
-PlayerWrapper.prototype._sortPlaylistByVotesCallback = function(a, b) {
-	var result = 0;
-	// We are sorting descending here, so the numbers are reversed.
-	if (a.votes > b.votes) {
-		result = -1;
-	}
-	else if(a.votes < b.votes) {
-		result = 1;
-	}
+            Array.prototype.splice.apply(
+                    playlist,
+                    parametersForSplice
+            );
+        }
+    }
+};
 
-	return result;
-}
+PlayerWrapper.prototype._sortPlaylistByVotesCallback = function (a, b) {
+    var result = 0;
+    // We are sorting descending here, so the numbers are reversed.
+    if (a.votes > b.votes) {
+        result = -1;
+    }
+    else if (a.votes < b.votes) {
+        result = 1;
+    }
+
+    return result;
+};
 
 module.exports = PlayerWrapper;
